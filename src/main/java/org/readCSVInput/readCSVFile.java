39e8fa6d4456eeea9b993cacc065b0868cba3920
MahipalSingh <mahisingh4896@gmail.com>
2023-05-19 16:11:18 +0530

    Comment added

--- src/main/java/org/readCSVInput/readCSVFile.java
+++ src/main/java/org/readCSVInput/readCSVFile.java
@@ -5,6 +5,8 @@ import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class readCSVFile {
     public static void main(String[] arg) {
@@ -33,5 +35,23 @@ public class readCSVFile {
             e.printStackTrace();
         }
         return map;
+    }
+
+    //This will be used for JSON parsing
+    public static Matcher regexMatcher(String regex, String line) {
+        line = "{ \r\n" + " \"employee\": { \r\n" + " \"name\": \"sonoo\", \r\n" + " \"salary\": 56000, \r\n" + " \"married\": true \r\n" + " } \r\n" + "} ";
+        regex = "name\":\\s+\"(.*?)\".*?:\\s+(\\d+).*?:\\s+(\\w+)";
+        // Create a Pattern object
+        Pattern r = Pattern.compile(regex, Pattern.DOTALL);
+        // Now create matcher object.
+        Matcher m = r.matcher(line);
+        if (m.find()) {
+            System.out.println("Name : " + m.group(1));
+            System.out.println("Salary: " + m.group(2));
+            System.out.println("Is Married ? : " + m.group(3));
+        } else {
+            System.out.println("NO MATCH");
+        }
+        return m;
     }
 }
